5efc6e6ddeac42ff737da88beaf645af2343a638
dahye1013 <alrean@naver.com>
2022-02-04 00:07:53 +0900

    [프로그래머스-고득점kit] 해시 - 완주하지 못한 선수

---
+++ 프로그래머스/고득점kit/해시(hash)/완주하지-못한-선수.js
@@ -0,0 +1,15 @@
+function solution(participant, completion) {
+  let map = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    const p = participant[i];
+    const c = completion[i];
+
+    map.set(p, (map.get(p) || 0) + 1);
+    map.set(c, (map.get(c) || 0) - 1);
+  }
+
+  for (const [key, value] of map.entries()) {
+    if (value > 0) return key;
+  }
+}
