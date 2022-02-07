ed98c2e33b1c31fb00c658d3d9f91453014068e6
dahye1013 <alrean@naver.com>
2022-02-07 23:34:05 +0900

    [JS-알고리즘] 01 section - 3개중 최소값 구하기

---
+++ JS-알고리즘-유형별/01-section/01-3개중최소값.js
@@ -0,0 +1,28 @@
+/**
+ * [최소값 구하기]
+ * 100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하세요.
+ * (정렬을 사용하지 않고 풀기)
+ */
+
+//[내 풀이법] ------------------------------------------------------
+
+function solution(...args) {
+  const arr = Array(...args);
+  return arr.reduce((acc, cur) => {
+    if (acc > cur) acc = cur;
+    return acc;
+  }, Number.MAX_SAFE_INTEGER);
+}
+
+console.log(solution(4, 7, 22));
+
+//[다른 풀이법] ------------------------------------------------------
+function solution(a, b, c) {
+  let answer = Number.MAX_SAFE_INTEGER;
+  if (a < b) {
+    answer = a;
+  } else answer = b;
+  if (b < c) answer = c;
+  return answer;
+}
+console.log(solution(2, 5, 1));
