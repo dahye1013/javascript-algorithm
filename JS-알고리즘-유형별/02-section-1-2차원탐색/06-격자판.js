13f068cda8b775cc954cf3261c54871da70642a4
dahye1013 <alrean@naver.com>
2022-04-30 22:21:39 +0900

    [JS-알고리즘 fix] 02 section 1,2차원배열 - 격자판

--- JS-알고리즘-유형별/02-section-1-2차원탐색/06-격자판.js
+++ JS-알고리즘-유형별/02-section-1-2차원탐색/06-격자판.js
@@ -1,50 +1,44 @@
 /**
  * @description
- * 기준점에서 네방향을 탐색해야한다.
- * 위(0, -1) 오른쪽(+1, 0) 아래(0, +1) 왼쪽(-1, 0)
- * dx = [0, 1, 0, -1]
- * dy = [-1, 0, +1, 0]
+ * N*N의 격자판이 주어지면 각 행의 합, 각 열의 합, 두 대각선의 합 중 가 장 큰 합을 출력한다.
+ *
+ * @return {number}
  */
-
 function solution(arr) {
-  let answer = Number.MIN_SAFE_INTEGER;
   const n = arr.length;
-  const dx = [0, 1, 0, -1];
-  const dy = [-1, 0, 1, 0];
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      let flag = 1;
-      for (let k = 0; k < dx.length; k++) {
-        // 행 좌표
-        const nx = i + dx[k];
-        // 열 좌표
-        const ny = j + dy[k];
-        if (
-          // 1. 가장자리를 참조하는 경우 제외
-          nx >= 0 &&
-          nx < n &&
-          ny > 0 &&
-          ny < n &&
-          // 2. 현재 지점보다 큰 것이 존재하는 경우
-          arr[nx][ny] >= arr[i][j]
-        ) {
-          // flag
-          flag = 0;
-          break;
-        }
+  let answer = Number.MIN_SAFE_INTEGER;
+  // 행의 합
+  let rowSum = 0;
+  // 열의 합
+  let columnSum = 0;
 
-        if (flag) answer += 1;
-      }
+  //(1) 2중 for문으로 각각 행렬의 합을 구해서, 가장 큰 값을 세팅한다.
+  for (let i = 0; i < arr.length; i++) {
+    rowSum = columnSum = 0;
+    for (let j = 0; j < arr.length; j++) {
+      rowSum += arr[i][j];
+      columnSum += arr[j][i];
     }
+    answer = Math.max(answer, rowSum, columnSum);
   }
+
+  rowSum = columnSum = 0;
+
+  //(2) 대각선 값을 구해서 세팅한다.
+  for (let i = 0; i < n; i++) {
+    rowSum += arr[i][i];
+    columnSum += arr[i][n - i - 1];
+  }
+  answer = Math.max(answer, rowSum, columnSum);
+
+  return answer;
 }
 
-const arr = [
+let arr = [
   [10, 13, 10, 12, 15],
   [12, 39, 30, 23, 11],
   [11, 25, 50, 53, 15],
   [19, 27, 29, 37, 27],
   [19, 13, 30, 13, 19],
 ];
-
 console.log(solution(arr));
